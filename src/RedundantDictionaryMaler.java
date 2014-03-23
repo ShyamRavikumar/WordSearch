d095d2d29fee8f4e40c4be7d001acda039e06b47
Shyam Ravikumar <shyam.ravikumar@gmail.com>
2014-03-23 21:31:09 +0530

    College Project
    Java Project to create a word search game

---
+++ src/RedundantDictionaryMaler.java
@@ -0,0 +1,22 @@
+/*import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+public class RedundantDictionaryMaler {
+	public static void main(String args[]) throws Exception
+	   {
+	      BufferedReader br = new BufferedReader(new FileReader("./src/dictionary.txt"));
+	      BufferedWriter bw = new BufferedWriter(new FileWriter("./src/out.txt"));
+	      String s = new String();
+	      while((s=br.readLine())!=null){
+	    	  if(s.length()>=4)
+	    		  bw.write(s+"\n");
+	    	  
+	      }
+	      br.close();
+	      bw.close();
+	   }
+
+}*/
---
+++ src/randtest.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+
+public class randtest {
+
+	
+	public static void main(String[] args) {
+		Random r = new Random();
+		
+	}
+
+}
