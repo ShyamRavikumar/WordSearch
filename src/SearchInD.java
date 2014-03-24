a79769ac48676ae700a61584d9fb6dd3b8fdafcc
josessandeep <josesasandeep@gmail.com>
2014-03-24 22:43:04 +0530

    nothing worth while

---
+++ src/SearchInD.java
@@ -0,0 +1,18 @@
+/*
+ 
+ 
+public class SearchInD {
+String input;
+// It takes arguments 1.String that has to be searched 
+// 2. String - first letter and the length of the string 
+
+	
+
+void Searching (String input)
+{
+	String[] user_input = new String[3];
+	
+}
+}
+
+*/
